ab81704a8cbeb8a7119bbe8f2b6ce3164fc4c2f6
Rutvik Patel <200305366@student.georgianc.on.ca>
2016-07-25 22:55:16 -0400

    Database Changed

--- Assignment-3/Models/Dishes.cs
+++ Assignment-3/Models/Dishes.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -15,22 +14,12 @@ namespace Assignment_3.Models
         // to display full sized image
         public string ImageUrl { get; set; }
         // to display thumbnail size image
-        [Display(Name = "Image")]
         public string ThumbnailUrl { get; set; }
         // to give description to an item
-        [Display(Name = "Dish Descriprtion")]
-        [Required(ErrorMessage = "Description Required")]
-        [StringLength(30, MinimumLength = 3, ErrorMessage = "Invalid")]
         public string Description { get; set; }
         // dish name
-        [Display(Name = "Dish Name")]
-        [Required(ErrorMessage = "DishName Required")]
         public string DishName { get; set; }
         // price of dish
-        [Display(Name = "Dish Price")]
-        [Required(ErrorMessage = "Dishprice Required")]
         public decimal price { get; set; }
-        
-
     }
 }
