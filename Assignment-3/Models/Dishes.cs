38aed47b2514e2e434ab6d9d607a02b0b29540dd
Rutvik Patel <200305366@student.georgianc.on.ca>
2016-07-28 15:39:10 -0400

    Changed Display name

--- Assignment-3/Models/Dishes.cs
+++ Assignment-3/Models/Dishes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,16 +11,20 @@ namespace Assignment_3.Models
         // Dish Id primary Key
         public int Id { get; set;}
         // Specifics food or drink 
+        [Display(Name = "Food Type")]
         public string Type { get; set; }
         // to display full sized image
+        [Display(Name = "Image")]
         public string ImageUrl { get; set; }
         // to display thumbnail size image
         public string ThumbnailUrl { get; set; }
         // to give description to an item
         public string Description { get; set; }
         // dish name
+        [Display(Name = "Dish Name")]
         public string DishName { get; set; }
         // price of dish
+        [Display(Name = "Dish Price")]
         public decimal price { get; set; }
     }
 }
